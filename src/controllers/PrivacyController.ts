09ff9f0007c45ea122c31d3d8f7120f89544772d
Asish <asishvenkat.a2004@gmail.com>
2026-04-21 14:13:52 +0530

    updates

--- src/controllers/PrivacyController.ts
+++ src/controllers/PrivacyController.ts
@@ -144,7 +144,7 @@ export class PrivacyController {
       success: true,
       message: 'Account deletion has been scheduled',
       deletionScheduledFor: deletionDate,
-      gracePeriod: '24-48 seconds',
+      gracePeriod: '24-48 hours',
       note: 'Open posted tasks are deleted first. Deletion is blocked only while active tasks or accepted applications exist. You can cancel this request until the scheduled deletion time by calling POST /api/v1/privacy/cancel-deletion.'
     });
   }
